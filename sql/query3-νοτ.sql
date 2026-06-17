8ddea006e870203f9b61d39be2f00a22edcc68cc
Stella Giatsou <113286475+stellagiatsou@users.noreply.github.com>
2026-06-18 00:16:24 +0300

    Update

--- sql/query3-νοτ.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	ffh.country_name,
-	ffh.country_id,
-	AVG(ffh.female_life_expectancy ) as avg_female_life_expectancy
-from fact_female_health ffh
-group by ffh.country_name, ffh.country_id 
-order by avg_female_life_expectancy desc
-limit 30;
