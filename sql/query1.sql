bc292ed09fd114e71f53cddb4c4a84e537bb85df
Stella Giatsou <113286475+stellagiatsou@users.noreply.github.com>
2026-06-20 02:18:40 +0300

    Update query1.sql

--- sql/query1.sql
+++ sql/query1.sql
@@ -1,4 +1,4 @@
-SELECT 
+/*SELECT 
 	dt.year_id, 
 	AVG(female_life_expectancy) AS "Average female life expectancy",
 	AVG(gender_inequality_index) as "Average gender inequality index"
@@ -6,4 +6,20 @@ from fact_female_health ffh
 join dim_time dt 
 	on dt.year_id = ffh.year_id 
 group by dt.year_id
-order by dt.year_id ASC;
+order by dt.year_id ASC;*/
+
+SELECT
+    year_id,
+    AVG(life_exp),
+    AVG(gii)
+FROM (
+    SELECT
+        year_id,
+        country_id,
+        AVG(female_life_expectancy) AS life_exp,
+        AVG(gender_inequality_index) AS gii
+    FROM fact_female_health_
+    GROUP BY year_id, country_id
+) t
+GROUP BY year_id
+ORDER BY t.year_id;
