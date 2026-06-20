8939107d2111c9f2436c73d88d08cfecd6f03c42
Stella Giatsou <113286475+stellagiatsou@users.noreply.github.com>
2026-06-20 18:43:45 +0300

    Update

--- sql/query3.sql
+++ sql/query3.sql
@@ -1,22 +1,27 @@
 SELECT
-    income_group,
-    AVG(life_exp) AS avg_female_life_expectancy,
-    AVG(gii) AS avg_gender_inequality_index
+    country_id,
+    MAX(legal_score) - MIN(legal_score) AS rights_improvement
 FROM (
-    SELECT
-        ffh.country_id,
-        dc.income_group,
-        ffh.year_id,
-        AVG(ffh.female_life_expectancy) AS life_exp,
-        AVG(ffh.gender_inequality_index) AS gii
-    FROM fact_female_health ffh
-    JOIN dim_country dc
-        ON dc.country_id = ffh.country_id
-    GROUP BY
-        ffh.country_id,
-        dc.income_group,
-        ffh.year_id
+    SELECT DISTINCT
+        f.country_id,
+        f.year_id,
+        (
+            (CASE WHEN l.can_choose_residence THEN 1 ELSE 0 END) +
+            (CASE WHEN l.can_travel_domestic THEN 1 ELSE 0 END) +
+            (CASE WHEN l.can_apply_passport THEN 1 ELSE 0 END) +
+            (CASE WHEN l.can_travel_abroad THEN 1 ELSE 0 END) +
+            (CASE WHEN l.get_job THEN 1 ELSE 0 END) +
+            (CASE WHEN l.no_gender_discrimination_employment THEN 1 ELSE 0 END) +
+            (CASE WHEN l.sexual_harassment_legislation THEN 1 ELSE 0 END) +
+            (CASE WHEN l.sexual_harassment_penalties THEN 1 ELSE 0 END) +
+            (CASE WHEN l.no_gender_discrimination_credit THEN 1 ELSE 0 END) +
+            (CASE WHEN l.can_sign_contract THEN 1 ELSE 0 END) +
+            (CASE WHEN l.can_register_business THEN 1 ELSE 0 END) +
+            (CASE WHEN l.can_open_bank_account THEN 1 ELSE 0 END)
+        ) AS legal_score
+    FROM fact_female_health f
+    JOIN dim_legal l
+        ON f.legal_id = l.legal_id
 ) t
-WHERE income_group IS NOT NULL
-GROUP BY income_group
-ORDER BY income_group;
+GROUP BY country_id
+ORDER BY rights_improvement DESC;
